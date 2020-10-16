501e80b9dbb2e981306a4b97c656dc00bac0db43
Youseop <66366941+youseop@users.noreply.github.com>
2020-10-16 21:06:15 +0900

    Updates

---
+++ precache-manifest.abd418f37308d0fc6e4fc3b55a7fb25b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ed480dbb3ac9d816fbb",
+    "url": "/BOJ_TESTCASE/static/css/main.94d6f6b7.chunk.css"
+  },
+  {
+    "revision": "4ed480dbb3ac9d816fbb",
+    "url": "/BOJ_TESTCASE/static/js/main.7cea7d1b.chunk.js"
+  },
+  {
+    "revision": "3baa5b6ad367e3199cf6",
+    "url": "/BOJ_TESTCASE/static/js/runtime~main.fd6e1722.js"
+  },
+  {
+    "revision": "6f3f9c0628b5fcbf0a25",
+    "url": "/BOJ_TESTCASE/static/js/2.2bb989a3.chunk.js"
+  },
+  {
+    "revision": "723ccf03b8c3f4482cf1f3817df22826",
+    "url": "/BOJ_TESTCASE/index.html"
+  }
+];
